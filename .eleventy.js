f2d3830b59661879251e2baf1a218c046fd734e3
mibesharam <k.bhavin.kb@gmail.com>
2020-10-24 23:49:31 +0530

    initial

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = eleventyConfig => {
+    // Copy our static assets to the output folder
+    eleventyConfig.addPassthroughCopy('styles');
+    eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addPassthroughCopy('images');
+  
+    // Returning something from the configuration function is optional
+    return {
+      dir: {
+        output: 'public'
+      }
+    };
+  };

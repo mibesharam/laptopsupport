10b0a5e14b88be1436456631364a263ee8f0a84d
mibesharam <k.bhavin.kb@gmail.com>
2020-11-29 14:41:48 +0530

    Mobile responsive

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,18 @@ function UpdateCurrentYearOnFooter(){
     $("#currYear").html( new Date().getFullYear() )
 }
 
-function showSpinner(){
-    const spinner = document.getElementById("spinner-wrapper");
-    spinner.style.visibility = "visible";
-}
+// function showSpinner(){
+//     const spinner = document.getElementById("spinner-wrapper");
+//     spinner.style.visibility = "visible";
+// }
 
 function hideSpinner(){
     const spinner = document.getElementById("spinner-wrapper");
     spinner.style.visibility = "hidden";
 }
+hideSpinner(); 
 
 window.onload = function(e){
     UpdateCurrentYearOnFooter();
-    hideSpinner();
+    
 }

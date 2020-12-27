b4c7ba648fd119c56d2b3203f0c79ead31b632b0
mibesharam <k.bhavin.kb@gmail.com>
2020-12-27 14:20:28 +0530

    about page completed

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -17,3 +17,17 @@ window.onload = function(e){
     UpdateCurrentYearOnFooter();
     
 }
+gsap.registerPlugin(ScrollTrigger);
+gsap.from('#bannerCarousel',{scrollTrigger: '#bannerCarousel',opacity: 0, duration: 1, y:-50});
+gsap.from('#bannerCarousel .carousel-caption',{ScrollTrigger: '#bannerCarousel .carousel-caption',opacity: 0, duration: 1, y:50, delay:0.5});
+
+gsap.from('#whyus div div', {scrollTrigger: '#whyus div div', opacity: 0, duration: 1, x:-50, stagger: 0.5});
+
+gsap.from('#diagnose .anim', {scrollTrigger: '#diagnose',opacity: 0, duration: 1, y:50, stagger: 0.5});
+
+gsap.from('#services .animRight', {scrollTrigger: '#services .animRight',opacity: 0, duration: 1, x:-50})
+gsap.from('#services .animLeft', {scrollTrigger: '#services .animLeft',opacity: 0, duration: 1, x:50})
+
+gsap.from('#whyUs .fadeInUp-stagger',{scrollTrigger: '#whyUs',opacity: 0, duration: 1, y:50, stagger: 0.3})
+
+gsap.from('#contactSection .fadeInRight',{scrollTrigger: '#contactSection',opacity: 0, duration: 1, x:-50, stagger: 0.1})
